df3e420c146b712425c8cbd6e110fedc3079bbc9
Sam Gammon <samdgammon@gmail.com>
2016-09-11 22:16:55 -0400

    yep

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -11,7 +11,6 @@
     $scope.message = "Please enter data first";
     $scope.checkTooMuch = function(){
       var str = $scope.foodList.trim();
-      var (console.log(str));
       if (str.length > 0) {
         var arr = str.trim().split(",").filter(Boolean);
         console.log(arr);
