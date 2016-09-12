3bb57db838648e4555ba62e284cbf13a49f29d8f
Sam Gammon <samdgammon@gmail.com>
2016-09-11 22:14:53 -0400

    getting a comment in there that is helpful to the user

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -11,6 +11,7 @@
     $scope.message = "Please enter data first";
     $scope.checkTooMuch = function(){
       var str = $scope.foodList.trim();
+      var (console.log(str));
       if (str.length > 0) {
         var arr = str.trim().split(",").filter(Boolean);
         console.log(arr);
