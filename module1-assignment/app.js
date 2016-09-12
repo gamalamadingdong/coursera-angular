e48861bc1296e2ab5537ecb6299576e2835fdafc
Sam Gammon <samdgammon@gmail.com>
2016-09-11 22:00:35 -0400

    module 1 complete

---
+++ module1-assignment/app.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheckerApp', [])
+  .controller('LunchCheckerCtrl', LunchCheckerCtrl);
+
+  LunchCheckerCtrl.inject = ['$scope'];
+
+  function LunchCheckerCtrl($scope){
+    $scope.foodList = "";
+    $scope.message = "Please enter data first";
+    $scope.checkTooMuch = function(){
+      var str = $scope.foodList.trim();
+      if (str.length > 0) {
+        var arr = str.trim().split(",").filter(Boolean);
+        console.log(arr);
+        if (arr.length <= 3){
+          $scope.message = "Enjoy!";
+        }else {
+          $scope.message = "Too Much!";
+        }
+      }
+    };
+
+  }
+
+
+})();
